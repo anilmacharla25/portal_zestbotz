90758be69b6df237d019dc32beaf415fe45c6887
anisha2902 <anishagazi29@gmail.com>
2024-12-09 19:27:34 +0530

    database changes

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=100,blank=False)
     email = models.EmailField()
-    subject = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100, default='No Subject')
     message = models.TextField()
 
     def __str__(self):
